2e56b68842792b377bef78a84620eff4fb76517c
Alexandr Neveroff <neveroff1976@gmail.com>
2023-01-26 23:51:52 +0200

    task-05 ready

---
+++ js/task-05.js
@@ -0,0 +1,10 @@
+const nameInput = document.querySelector("#name-input");
+const nameOutput = document.querySelector("#name-output");
+const startName = nameOutput.textContent;
+
+nameInput.addEventListener("input", (event) => {
+  nameOutput.textContent = event.currentTarget.value;
+  if (event.currentTarget.value === "") {
+    nameOutput.textContent = startName;
+  }
+});
