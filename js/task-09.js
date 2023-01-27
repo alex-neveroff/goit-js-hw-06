fed584c5ef85583b5ced89720557bde230a4e188
Alexandr Neveroff <neveroff1976@gmail.com>
2023-01-27 05:26:55 +0200

    task-09 ready

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
+const changeColorBtn = document.querySelector(".change-color");
+const body = document.querySelector("body");
+const declaredСolor = document.querySelector(".color");
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeColor = (event) => {
+  const newColor = getRandomHexColor();
+  body.style.backgroundColor = newColor;
+  declaredСolor.textContent = newColor;
+};
+
+changeColorBtn.addEventListener("click", changeColor);
