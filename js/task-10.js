976c74c31f59f00a1be70d075321914f6a1eb978
Alexandr Neveroff <neveroff1976@gmail.com>
2023-01-27 06:08:53 +0200

    task-10 ready

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,36 @@
+const numberBoxes = document.querySelector("#controls>input");
+const createButton = document.querySelector("[data-create]");
+const destroyButton = document.querySelector("[data-destroy]");
+const generalBox = document.querySelector("#boxes");
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const createBoxes = (amount) => {
+  const newBoxes = [];
+  for (let i = 0; i < amount; i += 1) {
+    const box = document.createElement("div");
+    box.style.width = `${30 + i * 10}px`;
+    box.style.height = `${30 + i * 10}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    newBoxes.push(box);
+  }
+  return newBoxes;
+};
+
+createButton.addEventListener("click", () => {
+  if (generalBox.childNodes.length > 0) {
+    generalBox.innerHTML = "";
+  }
+  generalBox.prepend(...createBoxes(numberBoxes.value));
+});
+
+const destroyBoxes = () => {
+  generalBox.innerHTML = "";
+  numberBoxes.value = "";
+};
+
+destroyButton.addEventListener("click", () => {
+  destroyBoxes();
+});
