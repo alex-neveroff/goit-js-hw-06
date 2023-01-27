2316d284b6adc520ab36efe88e2540c8257c8494
Alexandr Neveroff <neveroff1976@gmail.com>
2023-01-27 04:40:40 +0200

    task-08 ready

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", touchSubmit);
 
+function touchSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Усі поля повинні бути заповнені");
+  }
+
+  const login = { email: email.value, password: password.value };
+  console.log(login);
+  event.currentTarget.reset();
+}
